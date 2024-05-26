7f59e3ac61fca06b2b9b55649792cca9ca60d22f
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-27 00:06:39 +0300

    Pass and update already defined variable

--- index.js
+++ index.js
@@ -12,24 +12,32 @@ const colors = [
   "violet",
 ];
 
+let counter = 0;
+
 const changeBodyColor = async () => {
   const [tab] = await chrome.tabs.query({ active: true });
   chrome.scripting.executeScript(
     {
       target: { tabId: tab.id },
-      args: [colors],
-      func: (colors) => {
+      args: [colors, counter],
+      func: (colors, counter) => {
         // Runs inside the active TAB
         const colorIndex = Math.floor(Math.random() * colors.length);
         const color = colors[colorIndex];
         document.body.style.backgroundColor = color;
-        return { color };
+
+        counter++;
+        console.log("Counter: ", counter);
+        return { color, newCounter: counter };
       },
     },
     ([{ result }]) => {
       // Runs inside the POPUP
-      const { color } = result;
+      const { color, newCounter } = result;
+      counter = newCounter; // Updating already defined variable
+
       document.getElementById("colorEl").innerHTML = color;
+      document.getElementById("counterEl").innerHTML = counter;
     }
   );
 };
