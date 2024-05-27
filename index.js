2630b6e7432cf6f432df997fd1163fb0841e5915
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-27 15:45:20 +0300

    Shared values between active-tab/popup and background

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// Shared Values between POPUP and Background
+const shareValue = (type = "get", data = null) => {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage({ type, data }, (response) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+};
+
+(async () => {
+  try {
+    console.log("Shared Values: ", await shareValue("get"));
+
+    const res = await shareValue("set", { value: 2 });
+    console.log(res);
+
+    console.log("New Shared Values: ", await shareValue("get"));
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+})();
+
 // Runs inside the POPUP
 const colors = [
   "red",
@@ -20,7 +46,7 @@ const changeBodyColor = async () => {
     {
       target: { tabId: tab.id },
       args: [colors, counter],
-      func: (colors, counter) => {
+      func: async (colors, counter) => {
         // Runs inside the active TAB
         const colorIndex = Math.floor(Math.random() * colors.length);
         const color = colors[colorIndex];
@@ -36,6 +62,30 @@ const changeBodyColor = async () => {
         let sessionCounter = sessionStorage.getItem("counter") || 0;
         sessionStorage.setItem("counter", ++sessionCounter);
         console.log("Session Counter: ", sessionCounter);
+
+        // Shared Values between active TAB and Background
+        const shareValue = (type = "get", data = null) => {
+          return new Promise((resolve, reject) => {
+            chrome.runtime.sendMessage({ type, data }, (response) => {
+              if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+              } else {
+                resolve(response);
+              }
+            });
+          });
+        };
+
+        try {
+          console.log("Shared Values: ", await shareValue("get"));
+
+          const res = await shareValue("set", { value: 3 });
+          console.log(res);
+
+          console.log("New Shared Values: ", await shareValue("get"));
+        } catch (error) {
+          console.error("Error: ", error);
+        }
 
         return { color, counter };
       },
--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,37 @@
+// Runs in the background on every reload of the extension, or opening the browser
+// Variables defined here are global to the extension
+
+let counter = 0;
+let sharedValue = 777;
+
 chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {
-      alert("Hello from the service worker!");
-      console.log("Runs inside the active tab console!");
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      args: [counter],
+      func: (counter) => {
+        // Runs inside the active TAB when extension icon is clicked
+        counter++;
+        console.log("Counter: ", counter);
+
+        return { counter };
+      },
     },
-  });
+    ([{ result }]) => {
+      // Runs in the background
+      counter = result.counter;
+    }
+  );
 });
 
-console.log("Runs inside the extension console!");
+let sharedValues = { value: 1 };
+
+chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+  if (request.type === "get") {
+    sendResponse(sharedValues);
+  } else if (request.type === "set") {
+    sharedValues = request.data;
+    sendResponse({ status: "success" });
+  }
+  return true;
+});
